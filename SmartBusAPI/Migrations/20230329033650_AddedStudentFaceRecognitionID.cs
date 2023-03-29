a9313fb1e7ad8bc901443a6ca2f5bbda06d7a34f
malsabi <xxdarklord65xx@gmail.com>
2023-03-29 07:39:07 +0400

    Updated The seeding data in smartbusContext for student.FaceRecognitionID.

---
+++ SmartBusAPI/Migrations/20230329033650_AddedStudentFaceRecognitionID.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartBusAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedStudentFaceRecognitionID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FaceRecognitionID",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "FaceRecognitionID",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "FaceRecognitionID",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "FaceRecognitionID",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FaceRecognitionID",
+                table: "Students");
+        }
+    }
+}
