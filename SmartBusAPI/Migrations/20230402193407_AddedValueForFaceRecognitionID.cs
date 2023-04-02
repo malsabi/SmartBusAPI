183fbfdf4ad4b92df2c686d45aa6e6a3adf36d63
malsabi <xxdarklord65xx@gmail.com>
2023-04-02 23:34:38 +0400

    Added Migration for database

---
+++ SmartBusAPI/Migrations/20230402193407_AddedValueForFaceRecognitionID.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartBusAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedValueForFaceRecognitionID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "FaceRecognitionID",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "FaceRecognitionID",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "FaceRecognitionID",
+                value: 3);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "FaceRecognitionID",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "FaceRecognitionID",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "FaceRecognitionID",
+                value: 0);
+        }
+    }
+}
